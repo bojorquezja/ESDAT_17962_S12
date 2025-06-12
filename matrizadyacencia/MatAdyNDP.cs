277dd193c436fceed57eb270d564145dd40dd586
bojorquezja <bojorquez.ja@gmail.com>
2025-06-12 16:04:34 -0500

    AGREGA MATRIZ DE ADYACENCIA

---
+++ matrizadyacencia/MatAdyNDP.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matrizadyacencia {
+    internal class MatAdyNDP {
+        private int[,] matady;
+        public int Nodos {  get; }
+        public MatAdyNDP(int nodos) {
+            Nodos = nodos;
+            matady = new int[nodos, nodos];
+        }
+        public void Mostrar() {
+            for (int f = 0; f < matady.GetLength(0); f++) {
+                for (int c = 0; c < matady.GetLength(1); c++) {
+                    Console.Write(matady[f, c] + ", ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void AgregaArista(int origen, int destino, int peso) {
+            matady[origen, destino] = peso;
+            matady[destino, origen] = peso;
+        }
+    }
+}
--- matrizadyacencia/Program.cs
+++ matrizadyacencia/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using matrizadyacencia;
+
+Console.WriteLine("Ingrese cantidad de nodos:");
+int nodos = Convert.ToInt32(Console.ReadLine());
+MatAdyNDP mat = new MatAdyNDP(nodos);
+string[] resp = { "0", "0", "0" };
+while (resp.Length == 3) {
+    Console.WriteLine($"Ingrese Origen, Destino y Peso separados por comas:");
+    resp = Console.ReadLine().Split(",");
+    //0,1,2
+    if (resp.Length == 3) {
+        mat.AgregaArista(
+            Convert.ToInt32(resp[0]),
+            Convert.ToInt32(resp[1]),
+            Convert.ToInt32(resp[2])
+            );
+    }
+}
+
+mat.Mostrar();
