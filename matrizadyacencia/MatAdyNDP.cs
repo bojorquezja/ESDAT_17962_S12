28039cc1b375d6ddf82a10080be16c1a119d74e6
bojorquezja <bojorquez.ja@gmail.com>
2025-06-19 13:02:04 -0500

    maxpeso resuelto

--- matrizadyacencia/MatAdyNDP.cs
+++ matrizadyacencia/MatAdyNDP.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace matrizadyacencia {
     internal class MatAdyNDP {
         private int[,] matady;
-        public int Nodos {  get; }
+        public int Nodos { get; }
         public MatAdyNDP(int nodos) {
             Nodos = nodos;
             matady = new int[nodos, nodos];
@@ -40,6 +40,31 @@ namespace matrizadyacencia {
                     }
                 }
             }
+        }
+
+        //muestra nodos hasta que el peso acumulado alcance al valor de 
+        //maxpeso
+        public void DFSPesoMax(int origen, int maxpeso) {
+            bool[] visitados = new bool[Nodos];
+            int acum = 0;
+            DFSDFSPesoMaxUtil(origen, visitados, maxpeso, ref acum);
+        }
+        private void DFSDFSPesoMaxUtil(int origen, bool[] visitados, int maxpeso,
+            ref int acum) {
+            visitados[origen] = true;
+
+            Console.Write(origen + ",");
+            for (int c = 0; c < matady.GetLength(1); c++) {
+                if (matady[origen, c] > 0) {
+                    if (!visitados[c]) {
+                        acum = acum + matady[origen, c];
+                        if (maxpeso >= acum) {
+                            DFSDFSPesoMaxUtil(c, visitados, maxpeso, ref acum);
+                        }
+                    }
+                }
+            }
+
         }
     }
 }
--- matrizadyacencia/Program.cs
+++ matrizadyacencia/Program.cs
@@ -5,12 +5,12 @@ Console.WriteLine("Ingrese cantidad de nodos:");
 int nodos = Convert.ToInt32(Console.ReadLine());
 MatAdyNDP mat = new MatAdyNDP(nodos);
 string[] resp = { "0", "0", "0" };
-/*
+
 mat.AgregaArista(0, 1, 2);
 mat.AgregaArista(0, 2, 1);
 mat.AgregaArista(0, 3, 5);
 mat.AgregaArista(3, 1, 4);
-*/
+/*
 while (resp.Length == 3) {
     Console.WriteLine($"Ingrese Origen, Destino y Peso separados por comas (o cero para terminar):");
     resp = Console.ReadLine().Split(",");
@@ -23,7 +23,10 @@ while (resp.Length == 3) {
             );
     }
 }
-
-
+*/
+Console.WriteLine("ingrese peso maximo");
+int peso = Convert.ToInt32(Console.ReadLine());
 mat.Mostrar();
 mat.DFS(0);
+Console.WriteLine();
+mat.DFSPesoMax(0, peso);
